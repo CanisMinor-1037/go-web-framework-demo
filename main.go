5ced773aa158abbd84fd48ce7661c8fa04a3170a
CanisMinor <canisminor@qq.com>
2025-01-07 10:36:38 +0800

    统一返回结构

--- main.go
+++ main.go
@@ -11,35 +11,58 @@ import (
 
 type HelloReq struct {
 	g.Meta `path:"/" method:"get"`
-	Name string `v:"required" dc:"姓名"`
-	Age  int `v:"required" dc:"年龄"`
+	Name string `v:"required" json:"name" dc:"姓名"`
+	Age  int `v:"required" json:"age" dc:"年龄"`
 }
 
-type HelloRes struct {}
+type HelloRes struct {
+	Content string `json:"content" dc:"返回结果"`
+}
 type Hello struct {}
 
-func (Hello) Say(ctx context.Context, helloReq *HelloReq) (HelloRes *HelloRes, err error) {
-	r := g.RequestFromCtx(ctx)
-	r.Response.Writef("Hello, %s!, Your age is %d",
-		helloReq.Name,
-		helloReq.Age,
-	)
+type Response struct {
+	Message string `json:"message" dc:"消息提示"`
+	Data interface{} `json:"data" dc:"执行结果"`
+}
+
+func (Hello) Say(ctx context.Context, helloReq *HelloReq) (helloRes *HelloRes, err error) {
+	helloRes = &HelloRes{
+		Content: fmt.Sprintf(
+			"Hello, %s!, Your age is %d",
+			helloReq.Name,
+			helloReq.Age,
+		),
+	}
 	return
 }
 
-func ErrorHandler(r *ghttp.Request) {
+// 定义中间件
+func Middleware(r *ghttp.Request) {
 	r.Middleware.Next()
-	if err := r.GetError(); err != nil {
-		r.Response.Write("error occurs: ", err.Error())
-		return
+
+	var (
+		msg string
+		res = r.GetHandlerResponse()
+		err = r.GetError()
+	)
+
+	if err != nil {
+		msg = err.Error()
+	} else {
+		msg = "OK"
 	}
+	r.Response.WriteJson(Response{
+		Message: msg,
+		Data: res,
+	})
 }
+
 
 func main() {
 	fmt.Println("Hello, GF:", gf.VERSION)
 	s := g.Server()
 	s.Group("/", func(group *ghttp.RouterGroup) {
-		group.Middleware(ErrorHandler)
+		group.Middleware(Middleware)
 		group.Bind(
 			new(Hello),
 		)
