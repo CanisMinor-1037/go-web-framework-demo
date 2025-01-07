18f1fd656bcd63f50e6b084f9377e9fc874d667c
CanisMinor <canisminor@qq.com>
2025-01-07 10:10:03 +0800

    路由注册

--- main.go
+++ main.go
@@ -2,37 +2,37 @@ package main
 
 import (
 	"fmt"
+	"context"
 
 	"github.com/gogf/gf/v2"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/ghttp"
 )
 
-type User struct {
-	Name string `json:"name"`
-	Age  int    `json:"age"`
+type HelloReq struct {
+	g.Meta `path:"/" method:"get"`
+	Name string `v:"required" dc:"姓名"`
+	Age  int `v:"required" dc:"年龄"`
+}
+
+type HelloRes struct {}
+type Hello struct {}
+
+func (Hello) Say(ctx context.Context, helloReq *HelloReq) (HelloRes *HelloRes, err error) {
+	r := g.RequestFromCtx(ctx)
+	r.Response.Writef("Hello, %s!, Your age is %d",
+		helloReq.Name,
+		helloReq.Age,
+	)
+	return
 }
 
 func main() {
 	fmt.Println("Hello, GF:", gf.VERSION)
 	s := g.Server()
-	s.BindHandler("/", func(r *ghttp.Request) {
-			var user User
-			if err := r.Parse(&user); err != nil {
-				r.Response.Write(err.Error())
-				return
-			}
-			if user.Name == "" {
-				r.Response.Write("Name should not be empty")
-				return
-			}
-			if user.Age <= 0 {
-				r.Response.Write("invalid age value")
-				return
-			}
-			r.Response.Writef("Hello, %s!, Your age is %d",
-			user.Name,
-			user.Age,
+	s.Group("/", func(group *ghttp.RouterGroup) {
+		group.Bind(
+			new(Hello),
 		)
 	})
 	s.SetPort(8080)
